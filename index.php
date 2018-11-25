966f3ac7e1c5228e8c0254a5f939d94395e8ceac
ArturIBAS <44315676+ArturIBAS@users.noreply.github.com>
2018-11-25 18:30:33 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'Controller.php';
+$c=new Controller();
+
+
+if(filesize("/home/artur/BattleShip/JSON/player1.json")==0){
+require '/home/artur/BattleShip/HTML/formPlayer.php';
+$wr1=$c->action();//заполнение поля первого игрока
+}else{
+	if(filesize("/home/artur/BattleShip/JSON/player2.json")==0){
+	require '/home/artur/BattleShip/HTML/formPlayer.php';
+	$wr2=$c->action();//заполнение поля второго игрока
+	}
+}
+
+if(filesize("/home/artur/BattleShip/JSON/player1.json")!==0 && filesize("/home/artur/BattleShip/JSON/player2.json")!==0){
+	if($c->determineWinner()==0){//проверка на целостность флота у игроков
+		
+		$c->createHTML();//вывод своего и вражеского поля
+		$wr=$c->action();	
+		unset($_GET['chosenCell']);
+	}else{
+		if($c->determineWinner()==1) echo "Победил первый игрок!!!";
+		if($c->determineWinner()==2) echo "Победил второй игрок!!!";
+	}
+}
+
+?>
