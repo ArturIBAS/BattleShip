f9118f0cbb4c6b88d8bbbc1756d787f567f29302
ArturIBAS <44315676+ArturIBAS@users.noreply.github.com>
2018-11-26 19:30:39 +0400

    Third commit

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-
 require 'Controller.php';
 $c=new Controller();
-
-
 if(filesize("/home/artur/BattleShip/JSON/player1.json")==0){
 require '/home/artur/BattleShip/HTML/formPlayer.php';
 $wr1=$c->action();//заполнение поля первого игрока
@@ -13,7 +10,6 @@ $wr1=$c->action();//заполнение поля первого игрока
 	$wr2=$c->action();//заполнение поля второго игрока
 	}
 }
-
 if(filesize("/home/artur/BattleShip/JSON/player1.json")!==0 && filesize("/home/artur/BattleShip/JSON/player2.json")!==0){
 	if($c->determineWinner()==0){//проверка на целостность флота у игроков
 		
@@ -21,9 +17,15 @@ if(filesize("/home/artur/BattleShip/JSON/player1.json")!==0 && filesize("/home/a
 		$wr=$c->action();	
 		unset($_GET['chosenCell']);
 	}else{
-		if($c->determineWinner()==1) echo "Победил первый игрок!!!";
-		if($c->determineWinner()==2) echo "Победил второй игрок!!!";
+		if($c->determineWinner()==1) {
+			$c->recordResultOfGame(1);
+			echo "Победил первый игрок!";
+		}
+		
+		if($c->determineWinner()==2) {
+			$c->recordResultOfGame(2);
+			echo "Победил второй игрок!";
+		}
 	}
 }
-
 ?>
