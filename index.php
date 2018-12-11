6a78bfb23e9b7073e2a8187938527bf2f6bba37c
ArturIBAS <44315676+ArturIBAS@users.noreply.github.com>
2018-12-11 23:36:35 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
 <?php
-
+require 'connect_db.php';
 spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });
 
 $c=new Controller();
 
-if($c->determineWinner()==0){
 $c->action();
 GeneratorHTML::createHTML();
-}
-
-		if($c->determineWinner()==1) {
-			$c->recordResultOfGame(1);
-			echo "Победил первый игрок!";
-		}
-		
-		if($c->determineWinner()==2) {
-			$c->recordResultOfGame(2);
-			echo "Победил второй игрок!";
-		}
-	
 
 ?>
