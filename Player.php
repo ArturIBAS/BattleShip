15cbab90ecec1eaa3a3a4df6523b17eea32d2adb
ArturIBAS <44315676+ArturIBAS@users.noreply.github.com>
2018-12-18 20:20:26 +0400

    Update Player.php
    Убран массив объектов "клетка" (вынесен в класс Field)

--- Player.php
+++ Player.php
@@ -1,20 +1,11 @@
 <?php
 class Player{
 	public $name;
-	public $cells;
-	public $isAttack;
 
-	function __construct($namePlayer,$cellsPlayer,$statusAttack){
+	function __construct($namePlayer){
 		$this->name=$namePlayer;
-		$this->cells=$cellsPlayer;
-		$this->isAttack=$statusAttack;
 	}
+	
 
-	public function copyPlayer(&$player){//КОПИРУЕТ ОБЪЕКТ
-		$this->name=$player->name;
-		$this->cells=$player->cells;
-		$this->isAttack=$player->isAttack;
-		return $this;
-	}
 }
 ?>
